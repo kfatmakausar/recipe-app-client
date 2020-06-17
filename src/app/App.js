3f26d837854c64e5cce07083d161f841c1e3fb40
Fatma Kausar <kfatmakausar@gmail.com>
2020-06-17 15:09:58 -0400

    Added files

---
+++ src/app/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+import RoutesContainer from "../components/routes/RoutesContainer";
+import { NavBarContainer } from "../components/containers";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <NavBarContainer />
+        <header className="app-header">
+          <RoutesContainer />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
