0a82e1e04f2ee02be831902df7410006c9e3a421
Shahid <shahid.sarker@gmail.com>
2020-06-18 18:07:35 -0400

    add bookmark thunk and action

--- src/store/utilities/allBookmarks.js
+++ src/store/utilities/allBookmarks.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 //actiontypes
 const FETCH_ALL_BOOKMARKS = "FETCH_ALL_BOOKMARKS";
-// const ADD_BOOKMARK = "ADD_BOOKMARK";
+// const FETCH_USER_BOOKMARKS = "FETCH_USER_BOOKMARKS";
+const ADD_BOOKMARK = "ADD_BOOKMARK";
 
 //action creator
 const fetchAllBookmarks = (bookmarks) => {
@@ -12,11 +13,12 @@ const fetchAllBookmarks = (bookmarks) => {
   };
 };
 
-// const addBookmark = (recipeId) => {
-//     return {
-
-//     }
-// }
+const addBookmark = (bookmark) => {
+  return {
+    type: ADD_BOOKMARK,
+    payload: bookmark,
+  };
+};
 
 //thunk creators
 export const fetchAllBookmarksThunk = () => (dispatch) => {
@@ -27,11 +29,21 @@ export const fetchAllBookmarksThunk = () => (dispatch) => {
     .catch((err) => console.log(err));
 };
 
+export const addBookThunk = (recipeId) => (dispatch) => {
+  return axios
+    .post("/api/bookmarks", { data: { recipeId, userId: 1 } })
+    .then((res) => res.data)
+    .then((bookmark) => dispatch(addBookmark(bookmark)))
+    .catch((err) => console.log(err));
+};
+
 //reducer
 const reducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_ALL_BOOKMARKS:
       return action.payload;
+    case ADD_BOOKMARK:
+      return [...state, action.payload];
     default:
       return state;
   }
