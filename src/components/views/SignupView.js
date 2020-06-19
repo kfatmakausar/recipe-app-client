d15f802ce5872b7e16ba460af840490619d28279
ArslanM786 <ArslanMahmood321@gmail.com>
2020-06-19 08:10:34 -0400

    signup view

---
+++ src/components/views/SignupView.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+const SignupView = (props) => {
+  return (
+    <div className="signup">
+      <h1>Signup Here!</h1>
+    </div>
+  );
+};
+
+export default SignupView;
