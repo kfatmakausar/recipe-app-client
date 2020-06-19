200113c4413e983bd3ac7305dcf81e555ee2d91f
ArslanM786 <ArslanMahmood321@gmail.com>
2020-06-19 08:05:59 -0400

    setup

---
+++ src/components/views/HomeView.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+const HomeView = (props) => {
+  return (
+    <div className="home">
+      <h1>Welcome to Recipe Cook Book</h1>
+    </div>
+  );
+};
+
+export default HomeView;
